9b581002e815b169789725d6301eb7102e248ded
Daniel Müller <deso@posteo.net>
2025-02-17 10:08:17 -0800

    Add negative test for invalid #[test] attribute arguments
    Add a negative test for the compilation error reported when the #[test]
    attribute is used with unsupported arguments.

---
+++ tests/fail/test-invalid-args.rs
@@ -0,0 +1,8 @@
+// Copyright (C) 2025 Daniel Mueller <deso@posteo.net>
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+/// Unsupported inner attribute.
+#[test_fork::test(inner_test)]
+fn invalid_args() {}
+
+fn main() {}
