d4056d6e46605daad20b83fac0cfe1f152c2d718
Daniel Müller <deso@posteo.net>
2026-07-01 14:30:29 -0700

    Add test for error reporting of Result returning test
    Add a test that checks the error reporting behavior of an inner test
    that returns a Result value to indicate success/failure.

--- core/src/fork.rs
+++ core/src/fork.rs
@@ -247,13 +247,25 @@ fn fork_impl<T: Termination, R>(
 mod test {
     use super::*;
 
+    use std::io;
 
-    fn wait_for_child_output(child: Child) -> String {
+
+    /// Wait for a child success and report `stdout`.
+    fn wait_for_child_stdout(child: Child) -> String {
         let output = child.wait_with_output().expect("failed to wait for child");
         assert!(output.status.success());
-        let output = String::from_utf8(output.stdout).unwrap();
-        output
+        let stdout = String::from_utf8(output.stdout).unwrap();
+        stdout
     }
+
+    /// Wait for a child failure and report `stderr`.
+    fn wait_for_child_failure_stderr(child: Child) -> String {
+        let output = child.wait_with_output().expect("failed to wait for child");
+        assert!(!output.status.success());
+        let stderr = String::from_utf8(output.stderr).unwrap();
+        stderr
+    }
+
 
     #[test]
     fn fork_basically_works() {
@@ -273,7 +285,7 @@ mod test {
             "fork::test::child_output_captured_and_repeated",
             fork_id!(),
             |_| (),
-            wait_for_child_output,
+            wait_for_child_stdout,
             || {
                 fork_int(
                     "fork::test::child_output_captured_and_repeated",
@@ -287,6 +299,30 @@ mod test {
         )
         .unwrap();
         assert!(output.contains("hello from child"));
+    }
+
+    /// Check that errors reported by a `Result` returning test are
+    /// reported sensibly.
+    #[test]
+    fn child_error_output() {
+        let output = fork_int(
+            "fork::test::child_error_output",
+            fork_id!(),
+            |_| (),
+            wait_for_child_failure_stderr,
+            || {
+                fork_int(
+                    "fork::test::child_error_output",
+                    fork_id!(),
+                    |_| (),
+                    supervise_child,
+                    || io::Result::<()>::Err(io::Error::other("induced error")),
+                )
+                .unwrap()
+            },
+        )
+        .unwrap();
+        assert!(output.contains("induced error"));
     }
 
     #[test]
